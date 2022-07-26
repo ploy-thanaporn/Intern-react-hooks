171f8d12c87eb49de807125e7c03ba53e12b2873
ploy-thanaporn <tnpsp1501@gmail.com>
2022-07-26 15:23:07 +0700

    feat: random color by useState , useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  // useState
   const [count, setCount] = useState(0);
   const [title, setTitle] = useState("");
+  const [rgb, setRgb] = useState([0, 0, 0]);
+
+  // useEffect : life circle -> componentDidmount, componentDidupdate , componentWillUnmount
+  useEffect(() => {
+    const rgbElement = document.querySelector(".rgb");
+    rgbElement.style.color = `rgb(${rgb.join(",")})`;
+  });
 
   return (
-    <div className="App">
-      <button
-        onClick={() => {
-          setCount(count + 1);
-          setTitle("React Hook");
-        }}
-      >
-        ADD
-      </button>
-      <div className="">
-        UseState : {count}
-        <p>Title: {title}</p>
+    <div className="container">
+      <div className="App">
+        <div className="count">
+          <p>Title: {title}</p>
+          <p>UseState (count): {count}</p>
+
+          <button
+            onClick={() => {
+              setCount(count + 1);
+              setTitle("React Hook");
+            }}
+          >
+            ADD
+          </button>
+        </div>
+
+        <div className="rgb">
+          <p>Random RGB: {rgb.join(",")}</p>
+          <button
+            onClick={() =>
+              setRgb(
+                rgb.map(() => {
+                  return Math.floor(Math.random() * 256);
+                })
+              )
+            }
+          >
+            Random
+          </button>
+        </div>
       </div>
     </div>
   );
